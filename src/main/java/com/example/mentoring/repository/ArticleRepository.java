a32ff1e02f83dc0f1ffec948bf8158f246ecd96d
HoduUlmu <timewarp96@naver.com>
2022-06-18 17:41:44 +0900

    N + 1 문제 해결 - fetch join

--- src/main/java/com/example/mentoring/repository/ArticleRepository.java
+++ src/main/java/com/example/mentoring/repository/ArticleRepository.java
@@ -2,6 +2,12 @@ package com.example.mentoring.repository;
 
 import com.example.mentoring.domain.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ArticleRepository extends JpaRepository<Article, Long> {
+    @Override
+    @Query("select distinct a from Article a left join fetch a.comments")
+    List<Article> findAll();
 }
